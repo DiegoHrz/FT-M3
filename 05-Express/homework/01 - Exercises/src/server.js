6fbedc316bc55f51dd01a870d120b85ec7627b87
DiegoHrz <hernandezwdiego@gmail.com>
2023-09-26 23:54:41 -0500

    Add Homework 5 progress formatter

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -5,7 +5,18 @@ let publications = [];
 const server = express();
 
 server.use(express.json());
-server.use(morgan('dev'))
+
+server.put("/posts", (req, res) => {
+  const { author, title, contents } = req.params;
+  if (author && title && contents) {
+  }
+  return res
+    .status(404)
+    .json({
+      error:
+        "No se recibieron los parámetros necesarios para crear la publicación",
+    });
+});
 
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
 module.exports = { publications, server };
