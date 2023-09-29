184bc8c94f8d53d580f8a9a0f13402b4fe1560e6
DiegoHrz <hernandezwdiego@gmail.com>
2023-09-29 06:05:48 -0500

    Add Homework 6

--- 06-AsyncAwait/homework/01 - Exercises/exercise-two.js
+++ 06-AsyncAwait/homework/01 - Exercises/exercise-two.js
@@ -30,6 +30,12 @@ async function problemA() {
 
   // async await version
   // Tu código acá:
+  const stanzas = await Promise.all([
+    exerciseUtils.promisifiedReadFile("poem-two/stanza-01.txt"),
+    exerciseUtils.promisifiedReadFile("poem-two/stanza-02.txt"),
+  ]);
+  stanzas.forEach((stanza) => exerciseUtils.blue(stanza));
+  console.log("done");
 }
 
 async function problemB() {
@@ -46,6 +52,15 @@ async function problemB() {
 
   // async await version
   // Tu código acá:
+  //esto lo que hace es que por cada filenames = [poem-two/stanza-01.txt, poem-two/stanza-02.txt,...] devuelva una promesa
+  const promises = filenames.map((filename) =>
+    exerciseUtils.promisifiedReadFile(filename)
+  );
+  //Ahora esperar que todas las promesas se resuelvan despues q todos los archivos s resuelvan
+  const stanzas = await Promise.all(promises);
+  //ahora debemos devolverlas en forma aleatoria e imprimir a cada una en azul en la consola con el metodo .blue a cada una
+  stanzas.forEach((stanza) => exerciseUtils.blue(stanza));
+  console.log("done");
 }
 
 async function problemC() {
@@ -62,6 +77,11 @@ async function problemC() {
 
   // async await version
   // Tu código acá:
+  for (let i = 0; i < filenames.length; i++) {
+    const stanza = await exerciseUtils.promisifiedReadFile(filenames[i]);
+    exerciseUtils.blue(stanza);
+  }
+  console.log("done");
 }
 
 async function problemD() {
@@ -81,4 +101,13 @@ async function problemD() {
 
   // async await version
   // Tu código acá:
+  try{
+    for (let i = 0; i < filenames.length; i++){
+      const stanza = await exerciseUtils.promisifiedReadFile(filenames[i]);
+      exerciseUtils.blue(stanza)
+    }
+  }catch(err){
+    exerciseUtils.magenta(new Error(err))
+  }
+  console.log('done')
 }
